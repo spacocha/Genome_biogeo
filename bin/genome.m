a8f2626cf7f12800805e6ee6bbe20e61d2850e53
Sarah Preheim <sprehei1@jhu.edu>
2019-04-26 10:36:47 -0400

    Carrying capacity not actually working, removed some parts to troubleshoot.

--- bin/genome.m
+++ bin/genome.m
@@ -15,7 +15,7 @@ oxygen_bubble_rate=0;
 oxygen_source=0;
 carbon_source=0;
 nitrogen_source=0.0;
-carrying_capacity=50;
+carrying_capacity=10;
 %Currently there isn't any release of ammonia upon degredation
 %Maybe add this somehow with this from the amount of C removed 
 %or the amount of primary oxidaion with rates 1-5
@@ -350,6 +350,7 @@ function [merged_fluxes] = flux(~, merged_vector)
     div_fluxes = zeros(n_x, Cmax);
     total_div_increases = zeros(n_x, Cmax);
     room_for_growth = zeros(n_x);
+    actual_div_increases=zeros(n_x);
     % apply the oxygen bubbles
     conc_fluxes(:, s('O')) = conc_fluxes(:, s('O')) + oxygen_bubble_rate;
 
@@ -396,17 +397,18 @@ function [merged_fluxes] = flux(~, merged_vector)
 	for gx = 1 : Cmax
 		total_div_increases(x, gx)= total_div_increases(x, gx) + sum(times(ma_op_rates,div_mat(gx,3:end)));;
 	end
-	actual_div_increases(x)=min(room_for_growth(x), sum(total_div_increases(x,:)));
-	if actual_div_increases(x) < 0
-		actual_div_increases(x) = 0
-	end
+	%actual_div_increases(x)=min(room_for_growth(x), sum(total_div_increases(x,:)));
+	actual_div_increases(x)=sum(total_div_increases(x,:));
+	%if actual_div_increases(x) < 0
+%		actual_div_increases(x) = 0;
+%	end
 	%I should also change sum to max at some point
 	for gx = 1 : Cmax
-		if sum(total_div_increases(x, :)) > 0
+		%if sum(total_div_increases(x, :)) > 0
 			div_fluxes(x, gx) = div_fluxes(x, gx) + times(actual_div_increases(x), rdivide(sum(times(ma_op_rates,div_mat(gx,3:end))),sum(total_div_increases(x, :)))) - lambda*div(x,gx);
-		else
-			div_fluxes(x, gx) = div_fluxes(x, gx) - lambda*div(x,gx);
-		end
+		%else
+		%	div_fluxes(x, gx) = div_fluxes(x, gx) - lambda*div(x,gx);
+		%end
 	end
         
         % diffusion      
